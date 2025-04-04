bae7d1d2ee010213f98672adfec79ad3051add6a
vladislav_eroshkin <stiff_j@mai.ru>
2025-04-04 08:03:37 +0300

    added hk "shift+c" = backspace

--- src/main/java/Hotkeys.java
+++ src/main/java/Hotkeys.java
@@ -2,35 +2,36 @@ import com.melloware.jintellitype.JIntellitype;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 public class Hotkeys {
 
     public static void registerHotkeysForMouseClick() {
-        // Регистрация горячей клавиши Shift
         JIntellitype.getInstance().registerHotKey(1, JIntellitype.MOD_SHIFT, 0);
 
-        // Регистрация горячей клавиши "D"
         JIntellitype.getInstance().registerHotKey(2, 0, 'D');
 
         JIntellitype.getInstance().registerHotKey(3, JIntellitype.MOD_SHIFT, 'A');
+        JIntellitype.getInstance().registerHotKey(4, JIntellitype.MOD_SHIFT, 'C');
 
-        // Обработчик горячих клавиш
         JIntellitype.getInstance().addHotKeyListener(markCode -> {
             try {
                 Robot robot = new Robot();
 
                 if (markCode == 1) {
-                    // Эмулируем клик при нажатии Shift
                     robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
                     robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
                 } else if (markCode == 2) {
-                    // Эмулируем клик при нажатии "D"
                     robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
                     robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
                 }
                 else if (markCode == 3) {
                     killChromeProcess();
+                }
+                else if (markCode == 4) {
+                    robot.keyPress(KeyEvent.VK_BACK_SPACE);
+                    robot.keyRelease(KeyEvent.VK_BACK_SPACE);
                 }
             } catch (AWTException e) {
                 e.printStackTrace();
